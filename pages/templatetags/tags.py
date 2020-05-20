81d7fa2290b243a3de52fd065437a74e2667bdd2
cezcub <aarush.tiyyagura@gmail.com>
2020-05-20 13:29:17 -0500

    Added message to screen

--- pages/templatetags/tags.py
+++ pages/templatetags/tags.py
@@ -6,16 +6,18 @@ register = template.Library()
 
 @register.simple_tag
 def url_replace(request, field, value, direction=''):
-    dict_ = request.GET.copy()
+	dict_ = request.GET.copy()
+	if 'group' in dict_.keys():
+		dict_.pop('group')
 
-    if field == 'order_by' and field in dict_.keys():          
-      if dict_[field].startswith('-') and dict_[field].lstrip('-') == value:
-        dict_[field] = value
-      elif dict_[field].lstrip('-') == value:
-        dict_[field] = "-" + value
-      else:
-        dict_[field] = direction + value
-    else:
-      dict_[field] = direction + value
+	if field == 'order_by' and field in dict_.keys():
+		if dict_[field].startswith('-') and dict_[field].lstrip('-') == value:
+			dict_[field] = value
+		elif dict_[field].lstrip('-') == value:
+			dict_[field] = "-" + value
+		else:
+			dict_[field] = direction + value
+	else:
+		dict_[field] = direction + value
 
-    return urlencode(OrderedDict(sorted(dict_.items())))
+	return urlencode(OrderedDict(sorted(dict_.items())))
